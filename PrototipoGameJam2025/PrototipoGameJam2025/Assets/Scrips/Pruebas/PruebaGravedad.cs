5fe22fa931aacc26752b36ad90d44c6673ea6696
d3vsTech <127444582+d3vsTech@users.noreply.github.com>
2025-01-25 17:07:47 -0500

    update

---
+++ PrototipoGameJam2025/PrototipoGameJam2025/Assets/Scrips/Pruebas/PruebaGravedad.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PruebaGravedad : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody2D;
+    private float tiempoTranscurrido = 0f;
+    private float gravedadMaxima = 5f;
+
+    private float valorRuptura = 15f;
+
+    // Momento en el que sucederá el siguiente incremento.
+    private float proximoIncremento = 17f;
+
+    void Awake()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        tiempoTranscurrido += Time.deltaTime;
+
+        if(tiempoTranscurrido > valorRuptura)
+        {
+            if (tiempoTranscurrido >= proximoIncremento)
+            {
+                Debug.Log("Se cumplieron " + proximoIncremento + " segundos.");
+
+                // Aumentamos la gravedad si no hemos llegado al máximo
+                if (Rigidbody2D.gravityScale <= gravedadMaxima)
+                {
+                    Rigidbody2D.gravityScale += 0.1f;
+                }
+
+                // Fijamos el próximo incremento sumando 10 al anterior
+                proximoIncremento += 2f;
+            }
+        }
+    }
+}
